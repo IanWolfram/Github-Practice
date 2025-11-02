6c0bd69580d29a020d7163577784caeda4f5fa00
Ian Wolfram <wolfrait@mail.uc.edu>
2025-11-02 12:27:34 -0500

    Created new controller

---
+++ Controllers/returnNameController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Github_Practice.Controllers
+{
+    public class returnNameController : Controller
+    {
+
+        public IActionResult GetName()
+        {
+            var name = new { Name = "Ian Wolfram" };
+            return new JsonResult(name);
+        }
+    }
+}
